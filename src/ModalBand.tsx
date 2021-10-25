2ac957d8d3bac61d580a5788b46cd88075c6fab8
nils <48135649+Nilstrieb@users.noreply.github.com>
2021-10-25 08:59:20 +0200

    A-DF-Ω

--- src/ModalBand.tsx
+++ src/ModalBand.tsx
@@ -18,7 +18,9 @@ const ModalBand = ({ band, onClose }: Props) => {
                 <Modal.Header>{band.name}</Modal.Header>
                 <Modal.Body>
                     <img width="400" src={band.url_for_image_original} alt={band.name} />
-                    <div>{getBioText(band, 10000)}</div>
+                    {band.biographies.map((bio) => (
+                        <div key={bio.lang}>{bio.description}</div>
+                    ))}
                 </Modal.Body>
             </Modal>
         </div>
